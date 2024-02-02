1f89f894391738e34486fc238ee5731bff343412
Amartya Sen <amartyasen895@gmail.com>
2024-02-02 12:42:39 +0530

    toArray method

--- src/UNIT1/MethodType.java
+++ src/UNIT1/MethodType.java
@@ -3,7 +3,6 @@ package UNIT1;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class MethodType {
     public static void main ( String[] args ) {
@@ -11,31 +10,31 @@ public class MethodType {
         //Non - terminal -> filter , map , distinct , limit
         // Terminal -> collect , count , min , max , forEach , toArray
 
-        List < String > list = Arrays.asList ( "Car" , "bike" , "Bus" , "bike" , "car" , "Cycle" , "Cycle" );
-
-        List < String > collect = list.stream ( ).distinct ( ).collect ( Collectors.toList ( ) );
-        System.out.println ( collect );
+//        List < String > list = Arrays.asList ( "Car" , "bike" , "Bus" , "bike" , "car" , "Cycle" , "Cycle" );
+//
+//        List < String > collect = list.stream ( ).distinct ( ).collect ( Collectors.toList ( ) );
+//        System.out.println ( collect );
 
 //        list.stream ().distinct ().forEach ( System.out::println);
 
-        long itemCount = list.stream ( ).distinct ( ).count ( );
+//        long itemCount = list.stream ( ).distinct ( ).count ( );
 //        System.out.println (itemCount );
 
 //        long x = list.stream ().count ();
 
-        long y = list.stream ( ).distinct ( ).count ( );
+//        long y = list.stream ( ).distinct ( ).count ( );
 //        System.out.println (x );
 //        System.out.println (y );
 
-        list.stream ( ).limit ( 2 ).forEach ( System.out::println );
+//        list.stream ( ).limit ( 2 ).forEach ( System.out::println );
 
 
-        List < Integer > numLis = Arrays.asList ( 2 , 3 , 8 , 6 , 4 , 2 , 8 , 12 , 16 , 15 , 13 , 1 );
-        long evenCount = numLis.stream ( ).filter ( x -> x % 2 == 0 ).count ( );
-        System.out.println ( evenCount );
-
-        Optional < Integer > m = numLis.stream ( ).min ( Integer::compareTo );
-        System.out.println ( m.get ( ) );
+//        List < Integer > numLis = Arrays.asList ( 2 , 3 , 8 , 6 , 4 , 2 , 8 , 12 , 16 , 15 , 13 , 1 );
+//        long evenCount = numLis.stream ( ).filter ( x -> x % 2 == 0 ).count ( );
+//        System.out.println ( evenCount );
+//
+//        Optional < Integer > m = numLis.stream ( ).min ( Integer::compareTo );
+//        System.out.println ( m.get ( ) );
 
 
         //reduce
@@ -60,6 +59,16 @@ public class MethodType {
         );
 
         System.out.println ( sum.get ( ) );
+
+
+        //toArray
+
+        Object arr[] = list1.stream ().toArray ();
+        System.out.println (arr.length );
+        for ( Object x : arr )
+        {
+            System.out.println (x );
+        }
 
 
     }
